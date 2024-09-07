9d00c1326b85f09448e334871b49536b2e312201
sarunas.gincas <sarunikss@gmail.com>
2024-09-07 16:36:43 +0300

    Add class prop to Sider

--- fallout-ui/src/components/layout/sider.rs
+++ fallout-ui/src/components/layout/sider.rs
@@ -1,10 +1,16 @@
-use html::ChildrenProps;
 use yew::prelude::*;
 
+#[derive(Properties, PartialEq)]
+pub struct SiderProps {
+    #[prop_or_default]
+    pub class: Classes,
+    pub children: Children,
+}
+
 #[function_component(Sider)]
-pub fn sider(props: &ChildrenProps) -> Html {
+pub fn sider(props: &SiderProps) -> Html {
     html! {
-        <div class="bg-gray-800 w-64 py-2 px-4 h-full">
+        <div class={classes!("bg-gray-800", "w-64", "py-2", "px-4", "h-full", props.class.clone())}>
             { props.children.clone() }
         </div>
     }
