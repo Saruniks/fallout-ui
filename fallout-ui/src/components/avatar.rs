1c74e61d01c30b79ad0d228d1da34f92705a893a
sarunas.gincas <sarunikss@gmail.com>
2024-09-08 14:57:54 +0300

    Improve Avatar

--- fallout-ui/src/components/avatar.rs
+++ fallout-ui/src/components/avatar.rs
@@ -3,13 +3,27 @@ use yew::prelude::*;
 #[derive(Properties, PartialEq, Clone)]
 pub struct AvatarProps {
     pub text: String,
+    #[prop_or_default]
+    pub onclick: Option<Callback<MouseEvent>>,
+    #[prop_or_default]
+    pub classes: Classes,
 }
 
 #[function_component(Avatar)]
 pub fn avatar(props: &AvatarProps) -> Html {
+    let onclick = props.onclick.clone().unwrap_or_default();
+
     html! {
-        <div class="w-10 h-10 rounded-full flex items-center justify-center bg-gray-200 text-gray-700 font-bold">
-            {props.text.chars().next().unwrap_or_default().to_string()}
+        <div class="relative inline-block">
+            <div
+                class={classes!(
+                    "w-10", "h-10", "rounded-full", "flex", "items-center", "justify-center", "bg-gray-200", "text-gray-700", "font-bold", "cursor-pointer", "hover:shadow-lg", "transition-shadow", "duration-200",
+                    props.classes.clone() // Add additional classes
+                )}
+                {onclick}
+            >
+                {props.text.chars().next().unwrap_or_default().to_string()}
+            </div>
         </div>
     }
 }
